310b0c01491a1a6362d8cb673f4a5c3ee1bc227b
Phenyo <111151106+phenyo-droid@users.noreply.github.com>
2026-03-02 10:06:31 +0200

    Update expense_tracker

---
+++ expense_manager/lib/models/expense_models.dart
@@ -0,0 +1,91 @@
+class Expense {
+  final String id;
+  final double amount;
+  final String categoryId;
+  final String payee;
+  final String note;
+  final DateTime date;
+  final String tag;
+
+  Expense({
+    required this.id,
+    required this.amount,
+    required this.categoryId,
+    required this.payee,
+    required this.note,
+    required this.date,
+    required this.tag,
+  });
+
+  factory Expense.fromJson(Map<String, dynamic> json) {
+    return Expense(
+      id: json['id'] as String,
+      amount: (json['amount'] as num).toDouble(),
+      categoryId: json['categoryId'] as String,
+      payee: json['payee'] as String,
+      note: json['note'] as String,
+      date: DateTime.parse(json['date'] as String),
+      tag: json['tag'] as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'amount': amount,
+      'categoryId': categoryId,
+      'payee': payee,
+      'note': note,
+      'date': date.toIso8601String(),
+      'tag': tag,
+    };
+  }
+}
+
+class Category {
+  final String id;
+  final String name;
+
+  Category({
+    required this.id,
+    required this.name,
+  });
+
+  factory Category.fromJson(Map<String, dynamic> json) {
+    return Category(
+      id: json['id'] as String,
+      name: json['name'] as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+    };
+  }
+}
+
+class Tag {
+  final String id;
+  final String name;
+
+  Tag({
+    required this.id,
+    required this.name,
+  });
+
+  factory Tag.fromJson(Map<String, dynamic> json) {
+    return Tag(
+      id: json['id'] as String,
+      name: json['name'] as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+    };
+  }
+}
